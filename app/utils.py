da667db21e672e5e8226fb094fe42c4e20a007ba
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-08-14 12:23:29 +0100

    Added functions to return a JSON error

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,8 @@
+from flask import jsonify, make_response
+
+JSON_MIME_TYPE = 'application/json'
+
+
 def msisdn_formatter(msisdn):
     """
     Formats the number to the International format with the Nigerian prefix
@@ -11,3 +16,20 @@ def msisdn_formatter(msisdn):
     if msisdn[0] == '0':
         msisdn = msisdn[1:]
     return f"234{msisdn}"
+
+
+def json_response(data=None, status=200, headers=None):
+    headers = headers or {}
+    if 'Content-Type' not in headers:
+        headers['Content-Type'] = JSON_MIME_TYPE
+
+    if data:
+        payload = jsonify(data)
+    else:
+        payload = ''
+
+    return make_response(payload, status, headers)
+
+
+def response_error(code, message, status=400):
+    return json_response(data={'code': code, 'error': message}, status=status)
